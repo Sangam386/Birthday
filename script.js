d481715517791acda415b7dd3274458080977b41
Sangam386 <116751971+Sangam386@users.noreply.github.com>
2023-07-11 20:59:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,69 @@
-var images = ["https://iili.io/H6EO8E7.jpg"];
+var images = ['https://images.srkh.in/wp-content/uploads/2023/02/Happy-Propose-Day-Images-333-I-LOVE-YOU.jpg'];
 
 var currentIndex = 0;
 var totalClicks = 0;
 
 function randomizeImage() {
   let root = document.documentElement;
-  root.style.setProperty("--image", "url(" + images[currentIndex] + ")");
+  root.style.setProperty('--image', 'url(' + images[currentIndex] + ')');
   currentIndex++;
   if (currentIndex >= images.length) {
     currentIndex = 0;
   }
-  var puzzleItems = document.querySelectorAll("#puzz i");
+  var puzzleItems = document.querySelectorAll('#puzz i');
   for (var i = 0; i < puzzleItems.length; i++) {
-    puzzleItems[i].style.left =
-      Math.random() * (window.innerWidth - 100) + "px";
-    puzzleItems[i].style.top =
-      Math.random() * (window.innerHeight - 100) + "px";
+    puzzleItems[i].style.left = Math.random() * (window.innerWidth - 100) + 'px';
+    puzzleItems[i].style.top = Math.random() * (window.innerHeight - 100) + 'px';
   }
 }
 
 randomizeImage();
 
 function reloadPuzzle() {
-  var doneItems = document.querySelectorAll(".done");
+  var doneItems = document.querySelectorAll('.done');
   doneItems.forEach(function (element) {
-    element.classList.toggle("done");
+    element.classList.toggle('done');
   });
-  var droppedItems = document.querySelectorAll(".dropped");
+  var droppedItems = document.querySelectorAll('.dropped');
   droppedItems.forEach(function (element) {
-    element.classList.toggle("dropped");
+    element.classList.toggle('dropped');
   });
-  var allDoneElement = document.querySelector(".allDone");
-  allDoneElement.style = "";
-  allDoneElement.classList.toggle("allDone");
+  var allDoneElement = document.querySelector('.allDone');
+  allDoneElement.style = '';
+  allDoneElement.classList.toggle('allDone');
 }
 
-// Mobile functionality
-var puzzleItemsMobile = document.querySelectorAll("#puzz i");
+// mobile functionality
+var puzzleItemsMobile = document.querySelectorAll('#puzz i');
 puzzleItemsMobile.forEach(function (element) {
-  element.addEventListener("touchstart", function (event) {
-    event.preventDefault();
+  element.addEventListener('mousedown', function () {
     totalClicks++;
-    document.querySelector("#clicks").innerHTML = totalClicks;
-    element.classList.add("clicked");
+    document.querySelector('#clicks').innerHTML = totalClicks;
   });
-
-  element.addEventListener("touchend", function (event) {
-    event.preventDefault();
-    if (document.querySelector(".clicked")) {
-      document.querySelector(".clicked").classList.remove("clicked");
+  element.addEventListener('click', function () {
+    if (document.querySelector('.clicked')) {
+      document.querySelector('.clicked').classList.toggle('clicked');
+      element.classList.toggle('clicked');
+    } else {
+      element.classList.toggle('clicked');
     }
   });
 });
 
-var puzzleItemsDesktop = document.querySelectorAll("#puz i");
+var puzzleItemsDesktop = document.querySelectorAll('#puz i');
 puzzleItemsDesktop.forEach(function (element) {
-  element.addEventListener("click", function () {
-    if (document.querySelector(".clicked")) {
-      var clickedElement = document.querySelector(".clicked");
+  element.addEventListener('click', function () {
+    if (document.querySelector('.clicked')) {
+      var clickedElement = document.querySelector('.clicked');
       if (clickedElement.classList.contains(element.classList)) {
-        element.classList.add("dropped");
-        clickedElement.classList.add("done");
-        clickedElement.classList.remove("clicked");
+        element.classList.add('dropped');
+        clickedElement.classList.add('done');
+        clickedElement.classList.toggle('clicked');
 
-        if (document.querySelectorAll(".dropped").length === 9) {
-          document.querySelector("#puz").classList.add("allDone");
-          document.querySelector("#puz").style.border = "none";
-          document.querySelector("#puz").style.animation =
-            "allDone 1s linear forwards";
+        if (document.querySelectorAll('.dropped').length == 9) {
+          document.querySelector('#puz').classList.add('allDone');
+          document.querySelector('#puz').style.border = 'none';
+          document.querySelector('#puz').style.animation = 'allDone 1s linear forwards';
 
           setTimeout(function () {
             reloadPuzzle();
@@ -79,7 +75,7 @@ puzzleItemsDesktop.forEach(function (element) {
   });
 });
 
-// Mobile drag and drop
+// desktop drag and drop
 function allowDrop(ev) {
   ev.preventDefault();
 }
@@ -92,17 +88,14 @@ function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
 
-  if (ev.target.className === data) {
-    ev.target.classList.add("dropped");
-    document
-      .querySelector("." + data + "[draggable='true']")
-      .classList.add("done");
+  if (ev.target.className == data) {
+    ev.target.classList.add('dropped');
+    document.querySelector('.' + data + "[draggable='true']").classList.add('done');
 
-    if (document.querySelectorAll(".dropped").length === 9) {
-      document.querySelector("#puz").classList.add("allDone");
-      document.querySelector("#puz").style.border = "none";
-      document.querySelector("#puz").style.animation =
-        "allDone 1s linear forwards";
+    if (document.querySelectorAll('.dropped').length == 9) {
+      document.querySelector('#puz').classList.add('allDone');
+      document.querySelector('#puz').style.border = 'none';
+      document.querySelector('#puz').style.animation = 'allDone 1s linear forwards';
 
       setTimeout(function () {
         reloadPuzzle();
@@ -110,5 +103,4 @@ function drop(ev) {
       }, 1500);
     }
   }
-    }
-              
+}
